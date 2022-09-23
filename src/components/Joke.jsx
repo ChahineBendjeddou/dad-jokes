28f87ab155b8bfaa512533af7d9a33d5a6e56abc
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2022-09-23 12:14:49 +0100

    Adding Dynamic Colors and Emojis

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import "./Joke.css";
 
 const Joke = (state) => {
+  function getColorAndEmojie() {
+    if (state.votes >= 15)
+      return {
+        color: "#4CAF50",
+        emojie: "em em-rolling_on_the_floor_laughing",
+      };
+    else if (state.votes >= 12)
+      return { color: "#8BC34A", emojie: "em em-laughing" };
+    else if (state.votes >= 9)
+      return { color: "#CDDC39", emojie: "em em-smiley" };
+    else if (state.votes >= 6)
+      return { color: "#FFEB3B", emojie: "em em-slightly_smiling_face" };
+    else if (state.votes >= 3)
+      return { color: "#FFC107", emojie: "em em-neutral_face" };
+    else if (state.votes >= 0)
+      return { color: "#FF9800", emojie: "em em-confused" };
+    return { color: "#f44336", emojie: "em em-angry" };
+  }
+
   return (
     <div className="Joke">
       <div className="Joke-buttons">
@@ -9,7 +28,12 @@ const Joke = (state) => {
           class="fa-solid fa-arrow-up"
           onClick={() => state.handleVote(state.id, 1)}
         ></i>
-        <span className="Joke-votes">{state.votes}</span>
+        <span
+          style={{ borderColor: getColorAndEmojie().color }}
+          className="Joke-votes"
+        >
+          {state.votes}
+        </span>
         <i
           class="fa-solid fa-arrow-down"
           onClick={() => state.handleVote(state.id, -1)}
@@ -18,10 +42,7 @@ const Joke = (state) => {
       <div className="Joke-text">{state.text}</div>
 
       <div className="Joke-smiley">
-        <i
-          class="em em-rolling_on_the_floor_laughing"
-          aria-label="ROLLING ON THE FLOOR LAUGHING"
-        ></i>
+        <i className={getColorAndEmojie().emojie}></i>
       </div>
     </div>
   );
